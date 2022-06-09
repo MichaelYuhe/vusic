aeb531f404e34cd180ab3976e1d1c6f25ac3b8e2
MichaelYuhe <2312744987@qq.com>
2022-06-09 10:41:54 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import store from './store'
 import lazyPlugin from 'vue3-lazy'
 import loadingDirective from './components/Base/Loading/directive'
 import noResultDirective from './components/Base/NoResult/directive'
-
-
-// 引入全局样式文件
 import '@/assets/scss/index.scss'
 
 createApp(App).use(store).use(router).use(lazyPlugin, {
